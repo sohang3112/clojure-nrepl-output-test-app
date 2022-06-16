6c0a713ce6e14dd228c51b69bfc15a9942efbed8
Sohang  Chopra <sohang.chopra@mfilterit.com>
2022-06-16 14:19:58 +0530

    Main: Infinite print loop

--- src/example_clojure_app/core.clj
+++ src/example_clojure_app/core.clj
@@ -1,7 +1,11 @@
 (ns example-clojure-app.core
   (:gen-class))
 
+(def word (atom "Hello!"))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (while true
+    (println "Outside Eval:" @word)
+    (eval (println "Inside Eval"))))
